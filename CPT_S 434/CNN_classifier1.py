da61bd5b98bcdb30187079befed127d85218024a
ntsige <natnael.tsige@wsu.edu>
2023-04-23 23:11:08 -0700

    completed Project.

---
+++ CPT_S 434/CNN_classifier1.py
@@ -0,0 +1,144 @@
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+import torch.optim as optim
+import matplotlib.pyplot as plt
+import numpy as np
+from torch.utils.data import DataLoader
+import torchvision.utils as vutils
+
+# Hyper parameters
+num_classes = 6
+num_epochs = 10
+batch_size = 100
+learning_rate = 0.001
+
+# Device configuration
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+print('Using device:', device)
+
+# Image preprocessing
+train_transform = transforms.Compose([
+    transforms.RandomResizedCrop(224),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+])
+test_transform = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+])
+
+# Datasets
+train_dataset = torchvision.datasets.ImageFolder(root='C:/Users/Natek/Desktop/Project_434/Scene_detection/seg_train/seg_train', transform=train_transform)
+test_dataset = torchvision.datasets.ImageFolder(root='C:/Users/Natek/Desktop/Project_434/Scene_detection//seg_test/seg_test', transform=test_transform)
+
+# Data loaders
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+
+# CNN architecture
+class ConvNet(nn.Module):
+    def __init__(self, num_classes=6):
+        super(ConvNet, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        # Calculate the input size of the fully connected layer dynamically
+        with torch.no_grad():
+            self.fc_input_size = self._get_fc_input_size()
+        self.fc = nn.Linear(self.fc_input_size, num_classes)
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = out.view(out.size(0), -1)  # flatten the output of the second convolutional layer
+        out = self.fc(out)
+        return out
+
+    def _get_fc_input_size(self):
+        # Pass a dummy tensor through the convolutional layers and print its shape
+        x = torch.zeros(1, 3, 224, 224)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        return x.size(1) * x.size(2) * x.size(3)
+
+
+
+
+model = ConvNet(num_classes).to(device)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+total_step = len(train_loader)
+test_acc_list = []
+train_acc_list = []
+for epoch in range(num_epochs):
+    for i, (images, labels) in enumerate(train_loader):
+        images = images.to(device)
+        labels = labels.to(device)
+
+        # Forward pass
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+
+        # Backward and optimize
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
+                  .format(epoch + 1, num_epochs, i + 1, total_step, loss.item()))
+
+    # Train the model
+    with torch.no_grad():
+        correct = 0
+        total = 0
+        for images, labels in train_loader:
+            images = images.to(device)
+            labels = labels.to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+        print('Train Accuracy of the model on the {} training images: {} %'.format(total, 100 * correct / total))
+        train_acc_list.append(100 * correct / total)
+
+
+    # Test the model
+    model.eval()  # eval mode (batchnorm uses moving mean/variance instead of mini-batch mean/variance)
+    with torch.no_grad():
+        correct = 0
+        total = 0
+        for images, labels in test_loader:
+            images = images.to(device)
+            labels = labels.to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+        print('Test Accuracy of the model on the {} test images: {} %'.format(total, 100 * correct / total))
+        test_acc_list.append(100 * correct / total)
+
+
+plt.plot(train_acc_list, '-b', label='train acc')
+plt.plot(test_acc_list, '-r', label='test acc')
+plt.legend()
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+plt.xticks(rotation=60)
+plt.title('Accuracy ~ Epoch')
+plt.show()
